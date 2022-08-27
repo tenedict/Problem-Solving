41c4523b66b7a8782f31b4965ff8ae6cfecc66c1
tenedict <tenedict@gmail.com>
2022-08-27 22:08:06 +0900

    0827

---
+++ Python Practice/boe/20220827/1569.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+list_ = []
+for i in range(n):
+    li = []
+    a,b = map(int,input().split())
+    li.append(a)
+    li.append(b)
+    list_.append(li)
+
+print(list_)
+for a
+
