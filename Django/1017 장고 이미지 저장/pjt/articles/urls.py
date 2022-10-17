af60a1afb046a35a95bdca598527be915a404bac
tenedict <tenedict@gmail.com>
2022-10-17 16:51:34 +0900

    detail 완료

--- Django/1017 장고 이미지 저장/pjt/articles/urls.py
+++ Django/1017 장고 이미지 저장/pjt/articles/urls.py
@@ -6,5 +6,5 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
-    # path('detail/', views.)
+    path('<int:pk>/', views.detail, name='detail'),
 ]
